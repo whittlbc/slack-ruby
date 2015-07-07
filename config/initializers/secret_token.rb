d5b2ff2a3c192063bf69612c14cb4afa47bd83fd
Ben Whittle <benwhittle31@gmail.com>
2015-07-07 01:27:07 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SlackTest::Application.config.secret_key_base = 'b9fb33efc94add73b17a2eaaebd0ed4b691a21a51801fba1c1d6bdc4188d6e331ba70b092069ad838885ca8e44bdd0f57c067bfb6dd933977897e1c03903ff49'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SlackTest::Application.config.session_store :cookie_store, key: '_slackTest_session'
